0712d269f46b195b4263f917a577743be1770bfb
Joe Belcher <joe@techjargon.net>
2009-06-17 11:38:44 -0400

    updated generators

--- generators/rate_fu/rate_fu_generator.rb
+++ generators/rate_fu/rate_fu_generator.rb
@@ -2,10 +2,17 @@ class RateFuGenerator < Rails::Generator::Base
 
   def manifest 
     record do |m| 
+      # Create a migration from the template
       m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => 'rate_fu_migration'
+      # Create the directory app/views/ratings
       m.directory File.join('app/views/ratings')
-      m.template 'rating.html.erb', File.join('app/views/ratings', "_rating.html.erb")
-      m.template 'rate.rjs', File.join('app/views/ratings', "rate.rjs")
+      # Copy the file rate.rjs to /app/views/ratings/
+      m.file('rate.rjs', 'app/views/ratings/rate.rjs')
+      # Copy the partial _rating.html.erb to /app/views/ratings
+      m.file('_rating.html.erb', 'app/views/ratings/_rating.html.erb')
+      # Copy the stars.gif image into /public/images
+      m.file('stars.gif', 'public/images/stars.gif')
     end 
   end
 end
+
